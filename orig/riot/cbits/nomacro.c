7a19c17cadf29c322ab0cb6665b73afe0872cfb4
Sergej Pupykin <pupykin.s@gmail.com>
2010-04-07 15:03:22 +0400

    riot

---
+++ orig/riot/cbits/nomacro.c
@@ -0,0 +1,10 @@
+#include "nomacro.h"
+#include <Rts.h>
+#include <HsFFI.h>
+
+void nomacro_getyx(WINDOW *win, int *y, int *x)
+{
+    getyx(win, *y, *x);
+}
+
+HsInt hs_get_mb_cur_max () { return MB_CUR_MAX; }
